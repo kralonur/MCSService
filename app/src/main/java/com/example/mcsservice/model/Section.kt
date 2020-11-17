49dc04ec60408a097f7c26a81174f3fa7b2e60df
kralonur <kralonur1998@gmail.com>
2020-11-17 16:18:27 +0300

    Section class

---
+++ app/src/main/java/com/example/mcsservice/model/Section.kt
@@ -0,0 +1,19 @@
+package com.example.mcsservice.model
+
+import com.squareup.moshi.Json
+
+data class Section(
+    @Json(name = "id") val id: Int,
+    @Json(name = "created_at") val createdAt: String,
+    @Json(name = "updated_at") val updatedAt: String,
+    @Json(name = "name") val name: String,
+    @Json(name = "description") val description: String,
+    @Json(name = "subject_id") val subjectId: Int,
+    @Json(name = "section_type_id") val sectionTypeId: Int,
+    @Json(name = "section_type") val sectionType: SectionType,
+)
+
+data class SectionType(
+    @Json(name = "id") val id: Int,
+    @Json(name = "name") val name: String,
+)
