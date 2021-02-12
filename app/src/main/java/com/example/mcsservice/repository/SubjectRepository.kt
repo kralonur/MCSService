9e934aef70f58ef46ef0c20bd33a511495ae57b3
kralonur <kralonur1998@gmail.com>
2021-02-13 01:33:17 +0300

    Add ability to update local database from remote source

--- app/src/main/java/com/example/mcsservice/repository/SubjectRepository.kt
+++ app/src/main/java/com/example/mcsservice/repository/SubjectRepository.kt
@@ -14,6 +14,7 @@ import com.example.mcsservice.model.mapper.TaskRemoteToDbMapper
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import timber.log.Timber
 
 class SubjectRepository(context: Context) : BaseRepository() {
     private val api = NetworkService.subjectService
@@ -30,6 +31,116 @@ class SubjectRepository(context: Context) : BaseRepository() {
         db.materialDao().insertAll(materials)
         db.taskDao().insertAll(tasks)
     }
+
+    fun updateDatabase() = flowCall {
+        updateSubjectDatabase()
+    }
+
+    private suspend fun updateSubjectDatabase() {
+        val subjectsRemote = api.getAllSubjects().map { SubjectRemoteToDbMapper.map(it) }
+        val subjectsLocal = db.subjectDao().getAllSuspend()
+
+        val subjectsToAdd =
+            subjectsRemote.filter { remote -> subjectsLocal.find { local -> local.id == remote.id } == null }
+
+        val subjectsToUpdate =
+            subjectsRemote.filter { remote ->
+                subjectsLocal.find { local -> local.id == remote.id }
+                    ?.let { tL -> remote.updatedAt != tL.updatedAt } ?: false
+            }
+
+        val subjectIdChangeList = subjectsToAdd.toMutableList()
+            .apply { addAll(subjectsToUpdate) }.map { m -> m.id }.toList()
+
+        Timber.i("Subject update")
+        Timber.i(subjectsToAdd.toString())
+        Timber.i(subjectsToUpdate.toString())
+        updateSectionDatabase(subjectIdChangeList)
+
+        db.subjectDao().insertAll(subjectsToAdd)
+        db.subjectDao().updateAll(subjectsToUpdate)
+    }
+
+    private suspend fun updateSectionDatabase(subjectList: List<Int>) {
+        subjectList.forEach {
+            val sectionsRemote =
+                api.getSectionBySubject(it).map { m -> SectionRemoteToDbMapper.map(m) }
+            val sectionsLocal = db.sectionDao().getAllBySubjectIdSuspend(it)
+
+            val sectionsToAdd =
+                sectionsRemote.filter { remote -> sectionsLocal.find { local -> local.id == remote.id } == null }
+
+            val sectionsToUpdate =
+                sectionsRemote.filter { remote ->
+                    sectionsLocal.find { local -> local.id == remote.id }
+                        ?.let { tL -> remote.updatedAt != tL.updatedAt } ?: false
+                }
+
+            val sectionIdChangeList = sectionsToAdd.toMutableList()
+                .apply { addAll(sectionsToUpdate) }.map { m -> m.id }.toList()
+
+            Timber.i("Section update")
+            Timber.i(sectionsToAdd.toString())
+            Timber.i(sectionsToUpdate.toString())
+
+            updateMaterialDatabase(sectionIdChangeList)
+            updateTaskDatabase(sectionIdChangeList)
+
+            db.sectionDao().insertAll(sectionsToAdd)
+            db.sectionDao().updateAll(sectionsToUpdate)
+        }
+    }
+
+    private suspend fun updateMaterialDatabase(sectionList: List<Int>) {
+        sectionList.forEach {
+            val materialsRemote =
+                api.getMaterialBySection(it).map { m -> MaterialRemoteToDbMapper.map(m) }
+            val materialsLocal = db.materialDao().getAllBySectionIdSuspend(it)
+
+            val materialsToAdd =
+                materialsRemote.filter { remote -> materialsLocal.find { local -> local.id == remote.id } == null }
+
+            val materialsToUpdate =
+                materialsRemote.filter { remote ->
+                    materialsLocal.find { local -> local.id == remote.id }
+                        ?.let { tL -> remote.updatedAt != tL.updatedAt } ?: false
+                }
+
+            Timber.i("Material update")
+            Timber.i(materialsToAdd.toString())
+            Timber.i(materialsToUpdate.toString())
+
+            db.materialDao().insertAll(materialsToAdd)
+            db.materialDao().updateAll(materialsToUpdate)
+        }
+    }
+
+    private suspend fun updateTaskDatabase(sectionList: List<Int>) {
+        sectionList.forEach {
+            val tasksRemote =
+                api.getTaskBySection(it).map { m -> TaskRemoteToDbMapper.map(m) }
+            val tasksLocal = db.taskDao().getAllBySectionIdSuspend(it)
+
+            val tasksToAdd =
+                tasksRemote.filter { remote -> tasksLocal.find { local -> local.id == remote.id } == null }
+
+            val tasksToUpdate =
+                tasksRemote.filter { remote ->
+                    tasksLocal.find { local -> local.id == remote.id }
+                        ?.let { tL -> remote.updatedAt != tL.updatedAt } ?: false
+                }
+
+
+            Timber.i("Task update")
+            Timber.i(tasksToAdd.toString())
+            Timber.i(tasksToUpdate.toString())
+
+            db.taskDao().insertAll(tasksToAdd)
+            db.taskDao().updateAll(tasksToUpdate)
+        }
+    }
+
+    fun clearAllTables() = db.clearAllTables()
 
     fun getSubjectList() = db.subjectDao().getAll()
 
